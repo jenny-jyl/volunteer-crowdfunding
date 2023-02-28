7ee1e2952b81b14b4bd86eb4713f57f8015e87b2
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-02-28 18:01:30 +1100

    logo updated

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import logo from './logo.png';
 
 function Nav() {
     return (
         <nav className='main-nav-container'>
+            <img src={logo} alt="Help Me Go Logo" width="100" height="100"></img>
             <Link to="/" className="nav-item">Home</Link>
             <Link to="/project" className="nav-item">Project</Link>
             <Link to="/login" className="nav-item">Login</Link>
