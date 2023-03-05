d97281c6dd3491c31805b9c8ff29eee3467a7a47
jenny-jyl <114784070+jenny-jyl@users.noreply.github.com>
2023-03-05 16:30:35 +1100

    logo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '/Users/jennyli/volunteer-crowdfunding/src/components/images/logo.png';
 
 function Nav() {
 
@@ -33,7 +32,7 @@ function Nav() {
 
     return (
         <nav className='main-nav-container'>
-            <img src={logo} alt="Help Me Go Logo" width="100" height="100"></img>
+            <img src="https://i.ibb.co/34Zp6RL/logo.png" alt="Help Me Go Logo" width="100" height="100"></img>
             <Link to="/" className="nav-item">Home</Link>
             <Link to="/projects" className="nav-item">All Projects</Link>
             {
